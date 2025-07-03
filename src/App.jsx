b20da33e37f69fdbc5c7fc381c57b0ebe958627d
umar <amar.m.sheikh@gmail.com>
2025-07-03 13:30:16 +0500

    Set up App layout and routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,29 @@ import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
 import AllProducts from "./Components/Products/AllProducts/AllProducts";
 import SingleProducts from "./Components/SingleProducts/SingleProducts";
+import AppContext from "./utils/Context";
+import CategoryProducts from "./Components/Category/CategoryProducts";
+import BrandProducts from "./Components/Brand/BrandProducts";
+import Cart from "./Components/Cart/Cart";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      {/* <SingleProducts /> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<AllProducts />} />
-        <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/singleproducts" element={<SingleProducts />} /> */}
-      </Routes>
-      <NewsLetter />
-      <Footer />
+      <AppContext>
+        <Header />
+        <Cart />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/category/:id" element={<CategoryProducts />} />
+          <Route path="/brand/:id" element={<BrandProducts />} />
+          <Route path="/product/:id" element={<SingleProducts />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <NewsLetter />
+        <Footer />
+      </AppContext>
     </BrowserRouter>
   );
 }
